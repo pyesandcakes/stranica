924682f546b6fae1e9db008e04104f33c53d6707
Bruno Simunic <bruno.z.simunic@gmail.com>
2023-05-13 00:20:47 +0200

    loading

--- loader.js
+++ loader.js
@@ -1,7 +1,12 @@
-var loader= document.getElementById("preloader")
-    function loaddelay(){
-        loader.style.display="none";
+
+// if (!('hasCodeRunBefore' in localStorage)) {
+    var loader = document.getElementById("preloader")
+    function loaddelay() {
+        loader.style.display = "none";
     }
-    window.addEventListener("load", function(){
-        setTimeout(loaddelay,3000)
+    window.addEventListener("load", function () {
+        setTimeout(loaddelay, 3000)
     })
+//     localStorage.setItem("hasCodeRunBefore", true);
+// }
+
